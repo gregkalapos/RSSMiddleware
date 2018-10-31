4c1fc7421a2b375fbad577143e3b1d5837484f5a
Greg Kalapos <gergo@kalapos.net>
2018-10-31 11:52:54 +0100

    Adding projects

---
+++ AspNetCoreRssMiddleware/RssMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNetCoreRssMiddleware
+{
+    public class RssMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RssMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+        }
+    }
+}
